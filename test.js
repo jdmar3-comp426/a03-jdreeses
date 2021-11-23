d60122624a313aa574611248ac9b3f3959d08c6b
Josh Spiess <jdspiess@live.unc.edu>
2021-11-22 19:21:47 -0500

    finished mild 1

---
+++ test.js
@@ -0,0 +1,6 @@
+import {sumToString} from "./src/mild/mild_1.js";
+import { getIncreasingArray } from "./src/mild/mild_1.js";
+
+console.log(sumToString(3,4));
+
+console.log(getIncreasingArray(3,12));
