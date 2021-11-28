ed313a698d67839daad2d036aa9e32badbd8e32b
Josh Spiess <jdspiess@live.unc.edu>
2021-11-28 14:40:15 -0600

    medium1 Median(30/100)

--- src/medium/medium_1.js
+++ src/medium/medium_1.js
@@ -26,7 +26,9 @@ export function getSum(array) {
  * console.log(getMedian(array)); // 4.5
  */
 export function getMedian(array) {
-
+    let middle = Math.floor(array.length / 2);
+    array = [...array].sort((a, b) => a - b);
+  return array.length % 2 !== 0 ? array[middle] : (array[middle - 1] + array[middle]) / 2;
 }
 
 /**
