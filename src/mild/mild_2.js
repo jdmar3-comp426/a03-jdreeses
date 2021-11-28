a19a139e1a3b38222f8a362be08d105f625076f7
Josh Spiess <jdspiess@live.unc.edu>
2021-11-28 14:21:29 -0600

    fixed removeND

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -73,7 +73,7 @@ export function removeKey(object, key) {
  If only `removeKeyNonDestructive` was called, nothing would have changed.
  */
 export function removeKeyNonDestructive(object, key) {
-   var newObj = object;
+   var newObj = Object.assign({}, object);
    delete newObj[key];
    return newObj;
 }
