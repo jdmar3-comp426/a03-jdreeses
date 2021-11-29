5ea1f04b9afa683c897fc1136734ba683bf9f5b1
Josh Spiess <jdspiess@live.unc.edu>
2021-11-29 16:14:07 -0600

    some even & every even (68/100)

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -97,7 +97,11 @@ export const tenTimesFifty = () => {
  *    everyEven([1, 1, 0, 1, 1], x => x === 1)  <--  returns false
  */
 export const everyEven = (arr, test) => {
-
+    let newArr = [];
+    for(let i = 0; i <arr.length; i +=2 ) {
+        newArr.push(arr[i]);
+    }
+    return newArr.every(test);
 };
 
 
@@ -121,7 +125,11 @@ export const everyEven = (arr, test) => {
  *    someEven([0, 0, 0, 0, 0], x => x === 0)  <--  returns true
  */
 export const someEven = (arr, test) => {
-
+    let newArr = [];
+    for(let i = 0; i <arr.length; i +=2 ) {
+        newArr.push(arr[i]);
+    }
+    return newArr.some(test);
 };
 
 
